5706ee9b8784445ea691aa081cc308e5a326bff0
starkblaze01 <mp.pathela@gmail.com>
2019-04-04 14:46:26 +0530

    Add Navbar, Footer and Home Page

---
+++ client/src/components/layout/Home.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="landing">
+        <div className="dark-overlay landing-inner text-light">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center">
+                <h1 className="display-3 mb-4">Senz</h1>
+                <p className="lead"> Create a project! using Senz</p>
+                <hr />
+                <a href="register.html" className="btn btn-lg btn-info mr-2">
+                  Sign Up
+                </a>
+                <a href="login.html" className="btn btn-lg btn-light">
+                  Login
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Home;
